598b78164b3916382e468fa215ec814828300d05
Ash Lawson <108193997+Ashyy-code@users.noreply.github.com>
2024-07-01 11:38:28 +0100

    final commit on terms

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -7,13 +7,15 @@
     <p>Nothing I couldn't gather myself with a pen and paper and some free time. Its all public info you can see just by being in the server. </p>
     <p>If you wanted to, you could keep track of when people join a voice channel and write it down, then write it down when they leave. This would help you build a profile of the most popular days people are on over time. You could then use this info to help decide raid days/events. Well thats pretty much what this bot does but without the pen and paper. </p>
     <p>In terms of what the data looks like, it looks like this: Ashy#4319 : monday: 8:00am enter [Channel] : 8:00pm leave channel : 12 hours duration. (Like I said, info anyone in the server could just record themselves manually if they wanted to)</p>
-    <p>Also only in servers where the GamingDays bot is.</p>
+    <p>Also only in servers where the GamingDays bot is (Obviously)</p>
     <h2>Keyphrases..</h2>
     <p>When you say something that has a specific keyphrase in it, a counter against that keyphrase is incremented. The message isnt stored, the person who said it isnt stored. It just goes "oh i know that word.. bop!" and incerements the number of times that phrase was said.</p>
     <p>What does this data look like? well it looks like this: Phrase: M+ , Count: 7, Day: Monday. And thats it. Nothing about who said it, or what time it was said.</p>
     <p>This data will eventually be paired with the Best Day data, to help realise which days people are more likely to ASK for VC as well as join it.</p>
     <h2>No thanks</h2>
-    <p>Worry not, I am currently implementing an opt out and purge protocol for sensitive andy's. Soon you will be able to request to have your data purged by a simple whisper command to the bot, as well as opt out of being tracked. (Sorry but in order to know to ignore you actions, I will still kinda have to store your Discord Username so it knows to ignore you otherwise like.. how is it gonna know :shrug:)</p>
+    <p>If you still dont like the idea of being automatically tracked, dont worry I am working on an opt-out and purge system. You will be able to whisper the bot a command to have your data purged, and opt out of being automatically tracked (I will still track you myself with my pen and paper tho because its public info :) )</p>
+    <p>Jokes aside though, nothing sensitive is stored I will happily screenshare the database with you anytime you want just ask. All the info is public and transparent. </p>
+    <p>If you really do decide to make use of the opt-out system when its live, thats fine, but know that when it comes to scheduling events / raids, your records in here are your votes. Without your votes, you wont have an impact on the result. :(</p>
   </article>
 </template>
 
